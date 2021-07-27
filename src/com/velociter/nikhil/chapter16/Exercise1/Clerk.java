df28a292efe1eade06ce3957ef6bbf7513fda9bd
Nikhil-velociter <nikhilsahu.velociters@gmail.com>
2021-07-27 19:04:51 +0530

    committed by nikhil

---
+++ src/com/velociter/nikhil/chapter16/Exercise1/Clerk.java
@@ -0,0 +1,36 @@
+package com.velociter.nikhil.chapter16;
+public class Clerk implements Runnable {
+	// Constructor
+	public Clerk(Bank theBank) {
+		this.theBank = theBank; // Who the clerk works for
+		initiallyTransaction = null; // No transaction initially
+	}
+
+	// Receive a transaction
+	public void doTransaction(Transaction transaction) {
+		initiallyTransaction = transaction;
+	}
+
+	// The working clerk...
+	public void run() {
+		while (true) {
+			while (initiallyTransaction == null) { // No transaction waiting?
+				try {
+					Thread.sleep(150); // Then take a break...
+				} catch (InterruptedException e) {
+					System.out.println(e);
+				}
+			}
+			theBank.doTransaction(initiallyTransaction);
+			initiallyTransaction = null; // In-tray is empty
+		}
+	}
+
+	// Busy check
+	public boolean isBusy() {
+		return initiallyTransaction != null; // A full in-tray means busy!
+	}
+
+	private Bank theBank; // The employer - an electronic marvel
+	private Transaction initiallyTransaction ; // The in-tray holding a transaction
+}
