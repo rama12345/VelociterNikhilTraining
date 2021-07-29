e55174806884d175d184bbea1dbc2a19f3321d71
Nikhil-velociter <nikhilsahu.velociters@gmail.com>
2021-07-29 18:34:21 +0530

    changes class FileDateFormat by nikhil

--- src/com/velociter/nikhil/chapter10/FileDateFormat.java
+++ src/com/velociter/nikhil/chapter10/FileDateFormat.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Exercise2 {
+public class FileDateFormat {
 	public static void main(String args[]) throws IOException {
 
 		String[] monthNames = { "January", "February", "March", "April", "May", "June", "July", "August", "September",
@@ -45,7 +45,7 @@ public class Exercise2 {
 		// creating a file
 		FileWriter filewriter = new FileWriter("Date.txt");
 
-		String date = day + first + "," + monthString + "," + year;
+		String date = day + first + " " + monthString + " " + year;
 		// put the data into a file
 		for (j = 0; j < date.length(); j++) {
 			filewriter.write(date.charAt(j));
@@ -54,3 +54,4 @@ public class Exercise2 {
 		System.out.println("Write sucessfully");
 	}
 }
+
