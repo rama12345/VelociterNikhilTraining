111f0c8bfe15002d40bbbc0ed944f2604b2bd80d
Nikhil-velociter <nikhilsahu.velociters@gmail.com>
2021-07-29 18:35:50 +0530

    changes class FileSeperation by nikhil

--- src/com/velociter/nikhil/chapter10/FileSeperation.java
+++ src/com/velociter/nikhil/chapter10/FileSeperation.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Exercise1 {
+public class FileSeperation {
 	public static void main(String args[]) {
 		String dataStore;
 		try {
