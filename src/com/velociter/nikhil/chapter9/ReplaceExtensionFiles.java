1cd961d94d52b97721c5e238f9dba0cbfbcc77fe
Nikhil-velociter <nikhilsahu.velociters@gmail.com>
2021-08-06 16:42:07 +0530

    changes class ReplaceExtension.java by nikhil

--- src/com/velociter/nikhil/chapter9/ReplaceExtensionFiles.java
+++ src/com/velociter/nikhil/chapter9/ReplaceExtensionFiles.java
@@ -3,20 +3,25 @@ package com.velociter.nikhil.chapter9;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.util.Random;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ReplaceExtensionFiles {
+
 	public static boolean renameFileExtension(String source, String newExtension) {
 		String target;
 		// get the extension to new created string
 		String currentExtension = getFileExtension(source);
 		if (currentExtension.equals("")) {
+
 			// check the equality of currentExtension with empty space.
 			target = source + "." + newExtension;
 		} else {
+
 			// replace the all matches quote and pattern with the extension.
-			target = source.replaceFirst(Pattern.quote("." + currentExtension) + "$",
+			target = source.replaceFirst(Pattern.quote("." + currentExtension) ,
 					Matcher.quoteReplacement("." + newExtension));
 
 		}
@@ -24,40 +29,57 @@ public class ReplaceExtensionFiles {
 	}
 
 	// find and get the last index of extension.
-	public static String getFileExtension(String f) {
+	public static String getFileExtension(String fileName) {
 		String extension = "";
+
 		// find the last index of file extension.
-		int i = f.lastIndexOf('.');
+		int i = fileName.lastIndexOf('.');
 		// fetching the last index of substring and store into empty string.
-		if (i > 0 && i < f.length() - 1) {
-			extension = f.substring(i + 1);
+		if (i > 0 && i < fileName.length() - 1) {
+			extension = fileName.substring(i + 1);
 		}
 		return extension;
 	}
 
-	public static void main(String args[]) throws Exception {
-		// create string to store filename
-		String filename = "D:/myFile.txt";
-		// create file class object to store file
-		File txtFile = new File(filename);
-		FileOutputStream outputFile = null; // Place to store the stream reference
-		// check the file existence or not
-		if (txtFile.isFile()) {
-			// check the extension and rename it.
-			System.out.println(renameFileExtension(filename, "123"));
-			System.out.println("myFile.txt already exists.");
-		} else {
-			// Create the file stream
-			try {
-				// if file not exist it will create.
-				// Create the stream opened to append data
-				outputFile = new FileOutputStream(txtFile);
-				// check the extension and rename it.
-				System.out.println(renameFileExtension(filename, "123"));
-				System.out.println("myFile.txt output stream created");
-			} catch (FileNotFoundException e) {
-				e.printStackTrace(System.err);
+	public static void main(String[] args) {
+
+		String filename, result = "";
+		int i, lengthOfExtension;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter Your fileName : ");
+
+		if (input.hasNext("[A-Za-z.]*")) {
+			filename = input.nextLine();
+			lengthOfExtension = getFileExtension(filename).length();
+			// create file class object to store file
+			File txtFile = new File(filename);
+
+			// Place to store the stream reference
+			FileOutputStream outputFile = null;
+
+			// check the file existence or not
+			if (txtFile.isFile()) {
+				lengthOfExtension = getFileExtension(filename).length();
+				for (i = 1; i <= lengthOfExtension; i++) {
+					result += i + "";
+				}
+
+				System.out.print(result);
+				System.out.println(renameFileExtension(filename, result));
+				System.out.println("file already exists.");
+			} else {
+				// Create the file stream
+				try {
+					// if file not exist it will create.
+					// Create the stream opened to append data
+					outputFile = new FileOutputStream(txtFile);
+					System.out.println("file output stream created");
+				} catch (FileNotFoundException e) {
+					e.printStackTrace(System.err);
+				}
 			}
+		} else {
+			System.out.println("Please Enter Valid FileName.");
 		}
 	}
 }
