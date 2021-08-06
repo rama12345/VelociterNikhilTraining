618394e0a3e260d275bd63d2e8e85e99aa97a197
Nikhil-velociter <nikhilsahu.velociters@gmail.com>
2021-08-06 18:16:58 +0530

    changes class SequentialLineNumber by nikhil

--- src/com/velociter/nikhil/chapter15/SequentialLineNumber.java
+++ src/com/velociter/nikhil/chapter15/SequentialLineNumber.java
@@ -7,17 +7,20 @@ import java.io.IOException;
 
 public class SequentialLineNumber {
 	public static void main(String args[]) {
-		String textOfString = "My dog hasn’t got any nose.\n" + "How does your dog smell then?\n"
+		
+		   String textOfString = "My dog hasn’t got any nose.\n" + "How does your dog smell then?\n"
 				+ "My dog smells horrible.\n" + "How does your dog smell then?\n" + "My dog smells horrible.\n"
 				+ "How does your dog smell then?\n" + "My dog smells horrible.\n" + "How does your dog smell then?\n"
 				+ "My dog smells horrible.\n" + "How does your dog smell then?\n" + "My dog smells horrible.\n"
 				+ "How does your dog smell then?\n" + "My dog smells horrible.\n" + "How does your dog smell then?\n"
 				+ "My dog smells horrible.\n" + "How does your dog smell then?\n" + "My dog smells horrible.\n";
+		   
 		String lineOfString = null;
 		int count = 000;
 		String fileName = "E:/regulerEx1.txt";
 		BufferedWriter bufferWriter = null;
 		BufferedReader bufferReader = null;
+		
 		try {
 			// create the Object of FileWriter to write the data into file.
 			FileWriter fileWriter = new FileWriter(fileName);
@@ -27,6 +30,7 @@ public class SequentialLineNumber {
 			System.out.println("Successfully Write to File.......\n");
 			// close the writing operation.
 			bufferWriter.close();
+			
 			System.out.println("----------------Resulting String from File-----------");
 			// create the Object of FileReader to read the data from file.
 			FileReader fileReader = new FileReader(fileName);
@@ -38,6 +42,7 @@ public class SequentialLineNumber {
 				// print data from file line by line.
 				System.out.println("00" + count + " " + lineOfString);
 			}
+			
 			// close the reading operation.
 			bufferReader.close();
 		} catch (IOException e) {
